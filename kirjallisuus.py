f619678da81cfdd84d4c0d5d43aed956cd8aa1c6
Lauris Seebeck <lrseeb@utu.fi>
2024-04-01 02:10:17 +0300

    kirjallisuus kysymykset lisätty

--- kirjallisuus.py
+++ kirjallisuus.py
@@ -1,5 +1,12 @@
 kirjaKysymykset = {
-    "joku urheilu kysymys:\na. eka vaihtoehto\nb. tokavaihtoehto\nc. kolmasvaihtoehto\nd. neljäsvaihtoehto" : "a",
-    "toinen urheilu kysymys:\na. eka vaihtoehto\nb. tokavaihtoehto\nc. kolmasvaihtoehto\nd. neljäsvaihtoehto" : "b",
-    "kolmas urheilu kysymys:\na. eka vaihtoehto\nb. tokavaihtoehto\nc. kolmasvaihtoehto\nd. neljäsvaihtoehto" : "c",
+    "Mihin Turun Myllynsillan kuppeessa olevat pronssipatsaat tähyävät nimensä mukaan?:\na. tähtiin\nb. ruotsiin\nc. maahan\nd. pohjoiseen" : "a",
+    "Millä nimellä venäläinen Anna Gorenko saavutti mainetta runoilijana 1900-luvun alussa -\na. Anna Susimova\nb. Anna Ahmatova\nc. Anna Otsonova?" : "b",
+    "Kenen prhalaissyntyisen kirjailijan pää on ikuisesti yli kymmenmetriseksi hehkuvaksi patsaaksi, joka koostuu 42 liikkuvasta kerroksesta?:\na. Milan Kundera\nb. Rainer Maria Rilke\nc. Václav Havel\nd. Franz Kafka" : "d",
+    "Kenen päiväkirjojen mukaan nimetyssä kirjasarjassa seikkailee Greg Heffley -\na. vakoojan\nb. neropatin\nc. vampyyrin?" : "b",
+    "Kenen sarjakuvahahmon mukaan on nimetty Suomen sarjakuvaseuran vuosittain jakama palkinto?:\na. Muumipeikon\nb. Pekka Puupään\nc. Aku Ankan\nd. Wagnerin" : "b",
+    "Vuonna 1971 amerikkalainen astronautti jätti Kuuhun pienen metallipatsaan. Mitä se kuvaa -\na. limonadipulloa\nb. astronauttia\nc. maapalloa" : "b",
+    "Minkä nimistä uutislehteä Suomen puolustusvoimat julkaisee?:\na. Rauhan sanoma\nb. Joukkosanoma\nc. Ruotuväki\nd. Jalkaväki" : "c",
+    "Mikä Martti Suosalon ja Mika Nuojuan tähdittämä, Helsingin kaupunginteatterin komedia on rikkonut Suomen ennätyksen esityskerroissa mitattuna?:\na. Muovia meressä\nb. Roskia taskussa\nc. Kiviä kengissä\nd. Kiviä taskussa" : "d",
+    "Kenen taiteilijan studio Manhattanilla tunnettiin nimellä The Factory eli 'Tehdas' -\na. Jackson Pollockin\nb. Andy Warholin\nc. Yoko Onon\nd. Jenny Scobelin?" : "b",
+    "Mikä Ridley Scottin scifiklassikko pohjautuu Philip K. Dickin romaaniin Palkkionmetsästäjä?:\na. RoboCop\nb. Blade Runner\nc. Ex Machina\nd. Ghost in the Shell" : "b"
     }
