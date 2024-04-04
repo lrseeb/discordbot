3d5b347161cc6f5b233661e5b5ec3c12ae3621bb
Lauris Seebeck <lrseeb@utu.fi>
2024-04-04 13:00:05 +0300

    kaikki kysymykset lisätty :D

--- tiede.py
+++ tiede.py
@@ -1,5 +1,12 @@
 tiedeKysymykset = {
-    "joku tiede kysymys:\na. eka vaihtoehto\nb. tokavaihtoehto\nc. kolmasvaihtoehto\nd. neljäsvaihtoehto" : "a",
-    "toinen tiede kysymys:\na. eka vaihtoehto\nb. tokavaihtoehto\nc. kolmasvaihtoehto\nd. neljäsvaihtoehto" : "b",
-    "kolmas tiede kysymys:\na. eka vaihtoehto\nb. tokavaihtoehto\nc. kolmasvaihtoehto\nd. neljäsvaihtoehto" : "c",
+    "Nokkonen sisältää enemmän rautaa kuin pinaatti -\na. totta\nb. tarua?" : "a",
+    "Gepardi pystyy juoksemaan yli sata kilometriä tunnin aikana -\na. totta\nb. tarua?" : "b",
+    "Mikä valas upotti Essex-laivan, joka esiintyy elokuvassa Kamppailu merta vastaan -\na. ryhävalas\nb. kaskelotti\nc. harmaavalas?" : "b",
+    "Induktioliesi ei lämmittäessään nosta omaa lämpötilaansa, vaan lämpenee ainoastaan lämmittämänsä esineen vaikutuksesta -\na. totta\nb. tarua?" : "a",
+    "Mitä tarkoittaa oneromantia -\na. auravalokuvausta\nb. demonien manaamista\nc. unien tulkintaa" : "c",
+    "Mitä seuraavista perhoslajeista esiintyy Suomessa -\na. sulttaani\nb. monarkki\nc. ritari?" : "c",
+    "Mistä kehon osasta löydät vasara- ja alasinluut -\na. nenästä\nb. korvasta\nc. nilkasta\nd. polvesta?" : "b",
+    "Mitä ovat menneillä vuosisadoilla käytetyt koggi, karaveli ja kultteri?:\na. autoja\nb. pyöriä\nc. vaunuja\nd. laivoja" : "d",
+    "Minkä niminen internetyhteisö perustettiin vuonna 2009 luovien projektien rahoittamista varten?:\na. Kickstarter\nb. DeviantArt\nc. YouTube\nd. GoFundMe" : "a",
+    "Minkä aurinkokuntamme planeetan akseli on lähes vaakasuorassa eli melkein samansuuntainen kuin sen ratataso?:\na. Saturnukse\nb. Uranuksen\nc. Merkusiuksen\nd. Marsin" : "b"
     }
--- urheilu.py
+++ urheilu.py
@@ -1,5 +1,12 @@
 urheiluKysymykset = {
-    "joku urheilu kysymys:\na. eka vaihtoehto\nb. tokavaihtoehto\nc. kolmasvaihtoehto\nd. neljäsvaihtoehto" : "a",
-    "toinen urheilu kysymys:\na. eka vaihtoehto\nb. tokavaihtoehto\nc. kolmasvaihtoehto\nd. neljäsvaihtoehto" : "b",
-    "kolmas urheilu kysymys:\na. eka vaihtoehto\nb. tokavaihtoehto\nc. kolmasvaihtoehto\nd. neljäsvaihtoehto" : "c",
+    "Jarno Saarinen on tunnettu moottoripyöräilijä 1970-luvulta. Missä maassa on Jarno Saarisen puisto ja häntä esittävä pronssipatsas?:\na. Ranskassa\nb. Saksassa\nc. Italiassa\nd. Espanjassa" : "c",
+    "Minkä mitalin Lotta Lepistö voitti vuonna 2016 maantiepyöräilyn MM-kisoissa?:\na. kultamitalin\nb. hopeamitalin\nc. pronssimitalin" : "c",
+    "Freeganisminon parkourin kaltainen extremelaji, joka perustuu voltteihin ja hyppyihin -\na. totta\nb. tarua?" : "b",
+    "Mikä on Kouvola Rock'n Rollers -seuran laji?:\na. Roller derby\nb. ilmakitaran soitto\nc. Guitar Hero" : "a",
+    "Millä numerolla on joogassa symbolista arvoa, sillä se merkitsee Intian pyhien paikkojen määrää sekä pyhien kanavien määrää kehossa - numerolla\na. 72\nb. 108\nc. 122?" : "b",
+    "Mikä on todennäköisyys sille, että kahdella arpakuutiolla saa silmäluvuksi 7 - \na. 1/6\nb. 1/9\nc. 1/12" : "a",
+    "Mikä oli ensimmäinen ammattilaisurheiluliiga, jonka seuraajamäärä sosiaalisessa mediassa ylitti miljradin -\na. NHL\nb. NBA\nc. NFL?" : "b",
+    "Millä liikkeellä sinulla on tilastollisesti suurin todennäköisyys voittaa kivi-paperi-sakset-pelissä?:\na. kivellä\nb. saksilla\nc. paperilla" : "c",
+    "Mikä on vanhin tunnettu mailalla ja pallolla pelattava urheilu, jota pelataan yli sadassa maassa -\na. jääpallo\nb. maahockey\nc. kriketti?" : "b",
+    "Kuka oli ensimmäinen Ranskan ympäriajon voittanut amerikkalainen -\na. Greg LeMond\nb. Lance Armstrong\nc. Chris Carmichael?" : "a"
     }
